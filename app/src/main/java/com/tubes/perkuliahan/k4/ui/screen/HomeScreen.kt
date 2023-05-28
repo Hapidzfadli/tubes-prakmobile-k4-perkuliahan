0a2bce1a384192f22b758a31f1707ec9dfe051e9
Hapid Fadli <hapidzfadli@gmail.com>
2023-05-28 11:40:03 +0700

    membuat feture dashboard

--- app/src/main/java/com/tubes/perkuliahan/k4/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/tubes/perkuliahan/k4/ui/screen/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.tubes.perkuliahan.k4.ui.screen
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -20,7 +22,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -28,13 +32,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.tubes.perkuliahan.k4.R
-import com.tubes.perkuliahan.k4.ui.theme.DeepBlue
-import com.tubes.perkuliahan.k4.ui.theme.TextWhite
-import com.tubes.perkuliahan.k4.ui.theme.poppins
+import com.tubes.perkuliahan.k4.model.Feature
+import com.tubes.perkuliahan.k4.ui.theme.*
+import com.tubes.perkuliahan.k4.ui.utils.standardQuadFromTo
 import kotlinx.coroutines.delay
 import java.text.SimpleDateFormat
 import java.util.*
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(navController : NavHostController, modifier: Modifier = Modifier) {
     Box(
@@ -45,6 +50,42 @@ fun HomeScreen(navController : NavHostController, modifier: Modifier = Modifier)
         Column {
             GreetingSection ()
             CurrentDateTime()
+            FeatureSection(
+                features = listOf(
+                    Feature(
+                        title = "Dosen",
+                        R.drawable.teacher,
+                        BlueViolet1,
+                        BlueViolet2,
+                        BlueViolet3,
+                        12
+                    ),
+                    Feature(
+                        title = "Mahasiswa",
+                        R.drawable.graduated,
+                        LightGreen1,
+                        LightGreen2,
+                        LightGreen3,
+                        36
+                    ),
+                    Feature(
+                        title = "Mata Kuliah",
+                        R.drawable.matkul,
+                        OrangeYellow1,
+                        OrangeYellow2,
+                        OrangeYellow3,
+                        8
+                    ),
+                    Feature(
+                        title = "More",
+                        R.drawable.matkul,
+                        Beige1,
+                        Beige2,
+                        Beige3,
+                        50
+                    )
+                )
+            )
         }
     }
 }
@@ -120,6 +161,145 @@ fun CurrentDateTime() {
         while (true) {
             delay(1000) // Delay for 1 second
             currentDateTime = Calendar.getInstance().time // Update currentDateTime
+        }
+    }
+}
+
+@ExperimentalFoundationApi
+@Composable
+fun FeatureSection(features: List<Feature>) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Dashboard",
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier.padding(15.dp)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            items(features.size) {
+                FeatureItem(feature = features[it])
+            }
+        }
+    }
+}
+
+@Composable
+fun FeatureItem(
+    feature: Feature
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(7.5.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .background(feature.darkColor)
+    ) {
+        val width = constraints.maxWidth
+        val height = constraints.maxHeight
+
+        // Medium colored path
+        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
+        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+
+        val mediumColoredPath = Path().apply {
+            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
+            standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
+            standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
+            standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
+            standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+
+        // Light colored path
+        val lightPoint1 = Offset(0f, height * 0.35f)
+        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
+        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
+        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
+        val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
+
+        val lightColoredPath = Path().apply {
+            moveTo(lightPoint1.x, lightPoint1.y)
+            standardQuadFromTo(lightPoint1, lightPoint2)
+            standardQuadFromTo(lightPoint2, lightPoint3)
+            standardQuadFromTo(lightPoint3, lightPoint4)
+            standardQuadFromTo(lightPoint4, lightPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            drawPath(
+                path = mediumColoredPath,
+                color = feature.mediumColor
+            )
+            drawPath(
+                path = lightColoredPath,
+                color = feature.lightColor
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp)
+        ) {
+            Text(
+                text = feature.title,
+                style = TextStyle(
+                    color = Color.White,
+                    fontFamily = poppins,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                ),
+                lineHeight = 26.sp,
+                modifier = Modifier.align(Alignment.TopStart)
+            )
+            Icon(
+                painter = painterResource(id = feature.iconId),
+                contentDescription = feature.title,
+                tint = Color.White,
+
+                modifier = Modifier.align(Alignment.BottomStart).size(32.dp)
+            )
+            Text(
+                text = "view all",
+                style = TextStyle(
+                    color = Color.White,
+                    fontFamily = poppins,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 10.sp
+                ),
+                modifier = Modifier
+                    .clickable {
+                        // Handle the click
+                    }
+                    .align(Alignment.BottomEnd)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(ButtonBlue)
+                    .padding(vertical = 4.dp, horizontal = 10.dp)
+            )
+            Text(
+                text = feature.jumlah.toString(),
+                style = TextStyle(
+                    color = Color.White,
+                    fontFamily = poppins,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                ),
+                modifier = Modifier
+                    .align(Alignment.Center)
+
+            )
         }
     }
 }
--- app/src/main/java/com/tubes/perkuliahan/k4/ui/utils/PathUtil.kt
+++ app/src/main/java/com/tubes/perkuliahan/k4/ui/utils/PathUtil.kt
@@ -1,4 +1,13 @@
 package com.tubes.perkuliahan.k4.ui.utils
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Path
+import kotlin.math.abs
 
-class PathUtil {
+fun Path.standardQuadFromTo(from: Offset, to: Offset) {
+    quadraticBezierTo(
+        from.x,
+        from.y,
+        abs(from.x + to.x) / 2f,
+        abs(from.y + to.y) / 2f
+    )
 }
